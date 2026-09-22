e6de3b9fe033ad037a5e80a84e7738c05f4e6874
Juanma <juanmanuelsr98@gmail.com>
2026-09-22 10:57:42 +0200

    Ejercicio 1 y 2 añadidas correción

--- Ejerciciosprimeratanda/ejercicio1.php
+++ Ejerciciosprimeratanda/ejercicio1.php
@@ -2,14 +2,31 @@
 
 function calcularIva(float $precio, int $iva): float{
     //isset te comprueba si la variable es nula
+    //Esto estaría correcto 
     if (!isset($iva)){
         $iva = 21;
     }else{
-        $iva=($precio * $iva) / 100;
+        $iva=$precio * $iva / 100;
     }
 
     return $iva;
 }
+
+/** */
+/**pero la forma más correcta de hacerlo sería así
+*function calcularIva(float $precio, int $iva=21): float{
+*   
+*         $iva=$precio * $iva / 100;
+*
+*     return $iva;
+*     }
+*yo podría llamarla asi ahora echo calcularIva(2) y si le pongo precio 
+* en el parametro igual a 1 podría llamarla calcularIva().
+* pero tambien si solo quiero pasarle por parametro tiene que ser
+* calcularIva(iva: 30)
+*/
+
+
 
 echo "El iva es ", calcularIva(35.32,3);
 
--- Ejerciciosprimeratanda/ejercicio2.php
+++ Ejerciciosprimeratanda/ejercicio2.php
@@ -1,16 +1,23 @@
 <?php
-
-function sesion($nombre, $contraseña)
-{
-    if (empty($nombre) && empty($contraseña)) {
+define("NOMBRE", "admin");
+define("CONTRASEÑA", 1234);
+function sesion($nombre, $contraseña){
+    if (empty($nombre) || empty($contraseña)) {
 
         echo "El nombre o la contrasña está vacio. ERROR";
-    } elseif ($nombre == "admin" && $contraseña == 1234) {
+    } elseif ($nombre == NOMBRE && $contraseña == CONTRASEÑA) {
         echo "Inicio de sesión correcto, se encentra en admin";
-    }else{
+    } else {
         echo "Su sesión no esta resgistrada todavía";
     }
 }
 
+/**
+ * En html usuario = <?= $tesUser ?> --> Esto lanzaría un echo
+ * password = <?= $testPass ?> --> Esto lanzaría otro echo
+ * Esto esta pensado para poner solo una cosa, por ejemplo
+ * una variable o una funcion etc
+ * 
+ */
 
-sesion("admin",1234);
+sesion("admin", 1234);
