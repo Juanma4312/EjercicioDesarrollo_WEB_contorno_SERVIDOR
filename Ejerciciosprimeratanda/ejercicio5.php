6683cdf4391b6320c3ef4a27d34a0a8f5dbdff70
Juanma <juanmanuelsr98@gmail.com>
2026-09-23 09:51:03 +0200

    Ejercicio5 corregido de la  forma correcta

--- Ejerciciosprimeratanda/ejercicio5.php
+++ Ejerciciosprimeratanda/ejercicio5.php
@@ -1,5 +1,16 @@
 <?php
-function reverso(String $numero): String {
+function reverso(String $numero): String
+{
     return strrev($numero);
 }
 echo reverso("12345");
+
+/**
+ * functio reverso2(int $numero): int{
+ * $enString = strval($numero);
+ * $reverso = strrev($enstring);
+ * return intval($reverso);
+ * }
+ * 
+ * echo reverso(789)
+ */
