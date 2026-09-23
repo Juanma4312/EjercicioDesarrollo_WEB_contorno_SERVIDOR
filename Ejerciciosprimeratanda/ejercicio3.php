06a0f23e9f920ffa638e873e73fd8cd6b47dc67d
Juanma <juanmanuelsr98@gmail.com>
2026-09-23 09:27:38 +0200

    Ejercicio 3 resuelto y añadidas varias soluciones

--- Ejerciciosprimeratanda/ejercicio3.php
+++ Ejerciciosprimeratanda/ejercicio3.php
@@ -7,3 +7,15 @@ function sumatorio(int $a, int $b, int $c, int $d, int $e): int {
 }
 
 echo sumatorio(5,4,4,4,4);
+
+/**
+ * Seria correcto hacerlo con un procedimiento (no debe llevar return)
+ * function sumatorio(int $a, int $b, int $c, int $d, int $e): int {
+ *
+ *   echo "sumatorio de ($a,$b,$c,$d,$e) = ", $a + $b + $c + $d + $e;
+ *
+ *}
+ * 
+ * sumatorio(1,2,3,4,5);
+ *
+ */
